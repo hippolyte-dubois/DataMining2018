60ae893f6218667110f14687f67bd904cc8e4922
hippolyte-dubois <hippolyte.dubois@etu.unilim.fr>
2018-05-08 18:32:26 +0200

    error rate

--- src/Predicting.py
+++ src/Predicting.py
@@ -2,12 +2,13 @@ import warnings
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 
 import pandas as pd
-from sklearn import linear_model
+from sklearn import linear_model, metrics
 import numpy as np
 import matplotlib.pyplot as plt
-import json
+import json, io
 
 BINS_N = 50
+LOG=True
 
 # TODO
 # Test on Training, split it into two parts and do the schmilblik
@@ -28,15 +29,16 @@ def training(target, model, data):
     return model, model.score(X_train, Y_train), X_train, Y_train
 
 def testing(model, data):
-    X_test = data.iloc[:,:-5]
-    return model.predict(X_test)
+    X_cols = [4] + [x for x in range(7,data.shape[1])]
+    X_test = data.iloc[:,X_cols]
+    X_ID = data.loc[:,"Client"]
+    return model.predict(X_test), np.array(X_ID)
 
 def error(Y_train, Y_predict):
-    error=0
+    error=[]
     for i in range(len(Y_train)):
-        error+=(abs(Y_train[i]-Y_predict[i])/Y_train[i])
-    train_error=error/len(Y_train)*100
-    return train_error
+        error.append(abs(Y_train[i] - Y_predict[i]))
+    return error
 
 targets = ["CapaciteEmprunt", "PrevisionnelAnnuel"]
 models =   {"Linear": linear_model.LinearRegression(),
@@ -46,8 +48,10 @@ models =   {"Linear": linear_model.LinearRegression(),
 scores = {}
 
 all_data = load_data()
-training_data = all_data.sample(frac=2)
-test_data = all_data.substract(training_data)
+training_data = all_data.sample(frac=0.5)
+test_data = all_data[~all_data.isin(training_data)].dropna()
+
+output_f = open("predicted.txt","a")
 
 for t in targets:
     scores[t] = {}
@@ -58,10 +62,22 @@ for t in targets:
         scores[t][m] = model_score
         prediction = testing(model, data=test_data)
 
-        plt.hist(prediction, bins=BINS_N, color="blue", log=True, density=True)
+        #plt.tight_layout()
+        plt.subplot(1,2,1)
+        plt.hist(prediction[0], bins=BINS_N, color="blue", log=LOG, density=True)
         plt.ylabel("Predicted " + str(t))
-        plt.xlabel("Using "+str(m))
+        plt.xlabel("Values (histogram)")
+        #print("-"*20 + "\n" + t + ": " + m, file=output_f)
+        #for i in range(len(prediction[0])):
+        #    print(prediction[1][i] + ": " + str(prediction[0][i]), file=output_f)
+
+        error_rate = error(np.array(test_data[t]), prediction[0])
+        plt.subplot(1,2,2)
+        plt.hist(error_rate, bins=BINS_N, color="red", log=LOG, density=True)
+        plt.xlabel("Distance to the real value (histogram)")
         plt.savefig("./images/"+str(t)+"_"+str(m)+".png")
+        plt.clf()
+        plt.cla()
 
 with open("scores_prediction.json",'w+') as f_out:
     json.dump(scores,f_out, sort_keys=True, indent=4)
